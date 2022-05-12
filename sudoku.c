b5450984b156b90f8443c322b751ecc470f3ec61
EldritchBear <>
2022-05-12 17:32:07 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -78,17 +78,14 @@ int is_valid(Node* n){
 
   int p;
   
-  for(k = 0; k < 9 ; k++){
-    for(p=0;p<9;p++){
-      i=3*(k/3) + (p/3) ;
-      j=3*(k%3) + (p%3) ;
-      if(n->sudo[i][j] != 0){
-        aux = n->sudo[i][j];
-        arreglo[aux]++;
-        if (arreglo[aux] > 1)return 0;
-      }
-    }
+  for(k = 0; k < 9 ; k++)
+  for(p=0;p<9;p++){
+    i=3*(k/3) + (p/3) ;
+    j=3*(k%3) + (p%3) ;
+    printf("%d ",n->sudo[i][j]);
+    if(p%3 == 2) printf("\n");
   }
+  
   return 1;
 }
 
